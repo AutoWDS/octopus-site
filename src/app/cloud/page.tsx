bdb68feb37142e5235331b6608f21f9aea65e1cc
holmofy <hff1996723@163.com>
2023-06-27 13:50:09 +0800

    dir

--- src/app/cloud/page.tsx
+++ src/app/cloud/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Cloud() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      主页
+      Cloud
     </main>
   );
 }
